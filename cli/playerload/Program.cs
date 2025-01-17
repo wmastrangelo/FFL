bf68014bbe5da78655e39f40e43d385d60c80d90
wmastrangelo <walker.mastrangelo@gmail.com>
2025-01-16 21:57:48 -0500

    added table creation and data insertion

--- cli/playerload/Program.cs
+++ cli/playerload/Program.cs
@@ -15,21 +15,15 @@ namespace playerload
             .AddJsonFile("appsettings.json").Build();
 
             List<Player> players = LoadPlayers(args[0]);
-            try
+
+            string? conn = config.GetConnectionString("conn");
+            if (conn != null)
             {
-                MySqlConnection Connection = new(config.GetConnectionString("conn"));
-                Connection.Open();
-                var cmd = new MySqlCommand("SHOW TABLES;", Connection);
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader.GetString(0));
-                }
-                Connection.Close();
+                InsertPlayers(players, conn);
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e);
+                Console.WriteLine("conn string from config file failed to load");
             }
 
         }
@@ -56,9 +50,9 @@ namespace playerload
                     };
                     player.Team = string.IsNullOrEmpty(fields[2]) ? "FA" : fields[2];
                     player.Bye = string.IsNullOrEmpty(fields[3]) ? 0 : int.Parse(fields[3]);
+                    player.CBS = string.IsNullOrEmpty(fields[5]) ? 0 : int.Parse(fields[5]);
                     player.Sleeper = string.IsNullOrEmpty(fields[6]) ? 0 : int.Parse(fields[6]);
                     player.RTSports = string.IsNullOrEmpty(fields[7]) ? 0 : int.Parse(fields[7]);
-
                     players.Add(player);
                 }
                 parser.Close();
@@ -69,10 +63,73 @@ namespace playerload
             }
             return players;
         }
-        protected void InsertPlayers(List<Player> players)
+        static void InsertPlayers(List<Player> players, string conn)
         {
 
+
+            MySqlConnection Connection = new(conn);
+            Connection.Open();
+            MySqlTransaction tx = Connection.BeginTransaction();
+
+
+            var cmd = Connection.CreateCommand();
+            cmd.Transaction = tx;
+            try
+            {
+                string tablename = "Players - " + DateTime.Today;
+                 cmd.CommandText = "CREATE TABLE `" + tablename + "` (" +
+                    "`Rank` int DEFAULT NULL," +
+                    "`Name` varchar(50) DEFAULT NULL," +
+                    "`Team` varchar(5) DEFAULT NULL," +
+                    "`Bye` int DEFAULT NULL," +
+                    "`POS` varchar(5) DEFAULT NULL," +
+                    "`CBS` int DEFAULT NULL," +
+                    "`Sleeper` int DEFAULT NULL," +
+                    "`RTSports` int DEFAULT NULL," +
+                    "`AVG` float DEFAULT NULL" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+                    cmd.ExecuteNonQuery();
+
+                players.ForEach(player =>
+                {
+                    
+                    cmd.CommandText = "insert into `"+ tablename +"` set " +
+                    "`Rank` = @rank, " +
+                    "`Name` = @name, " +
+                    "`Team` = @team, " +
+                    "`Bye` = @bye, " +
+                    "`POS` = @pos, " +
+                    "`CBS` = @cbs, " +
+                    "`Sleeper` = @sleeper, " +
+                    "`RTSports` = @rtsports, " +
+                    "`AVG` = @avg;";
+                    cmd.Parameters.AddWithValue("@rank", player.Rank);
+                    cmd.Parameters.AddWithValue("@name", player.Name);
+                    cmd.Parameters.AddWithValue("@team", player.Team);
+                    cmd.Parameters.AddWithValue("@bye", player.Bye);
+                    cmd.Parameters.AddWithValue("@pos", player.POS);
+                    cmd.Parameters.AddWithValue("@cbs", player.CBS);
+                    cmd.Parameters.AddWithValue("@sleeper", player.Sleeper);
+                    cmd.Parameters.AddWithValue("@rtsports", player.RTSports);
+                    cmd.Parameters.AddWithValue("@avg", player.AVG);
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                });
+                tx.Commit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                try
+                {
+                    tx.Rollback();
+                }
+                catch (Exception exRollback)
+                {
+                    Console.WriteLine("Rollback error: " + exRollback);
+                }
+            }
         }
     }
- 
+
 }
